6bd682b1e354d42ffe9f45828bb4c40b21f879a9
Marcelo Caleb Júlio Ofice <marcelocalebjulioo@gmail.com>
2025-09-17 07:37:46 +0200

    CSS global

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-export function App() {
+import { GlobalStyle } from './styles/global';
+
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+   <div className="App">
+      <h1>Hello World</h1>   
+      <GlobalStyle/>  
     </div>
   );
-}
+};
+
+export default App;
+
